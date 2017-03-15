84e38f46d4fb32494b283c985135175712fca288
DreadKnight <dk.vali@gmail.com>
2017-03-15 13:42:22 +0200

    OpenShift is cancer.
    and damn git submodules as well.

---
+++ game/server/gamemanager.js
@@ -0,0 +1,14 @@
+// Game manager module
+var games = [];
+
+function Game(playerA, playerB) {
+  this.playerA = playerA;
+  this.playerB = playerB;
+};
+
+exports.startGame = function(playerA, playerB)
+{
+  var newGame = new Game(playerA, playerB);
+  games.push(newGame);
+  console.log("Starting a 2 players match");
+}
