67b96bd17878ff16d81e27f8a2de3ef6e149c1a1
DreadKnight <dk.vali@gmail.com>
2019-03-14 08:21:39 +0200

    bogus

--- blog/subscribe.php
+++ blog/subscribe.php
@@ -16,7 +16,7 @@
 	<div id="mce-responses" class="clear">
 		<div class="response" id="mce-error-response" style="display:none"></div>
 		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups -->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_32f1406080b23a82ecf9e8d6b_94b74a4105" tabindex="-1" value=""></div>
     </div>
 </form>
