8a660c89504c7649f785889a5849867d3f8504fe
DreadKnight <dk.vali@gmail.com>
2019-03-14 23:06:55 +0200

    proper form labels

--- blog/subscribe.php
+++ blog/subscribe.php
@@ -4,7 +4,8 @@
 <div id="mc_embed_signup">
 <form action="//FreezingMoon.us7.list-manage.com/subscribe/post?u=32f1406080b23a82ecf9e8d6b&amp;id=94b74a4105" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-	<div style="display: inline-block; padding-top: 8px; padding-right: 50px;"><span style="font-size: 1.1em;">Be part of our mailing list and receive news bulletins!</span><br><br>
+	<div style="display: inline-block; padding-top: 8px; padding-right: 50px;">
+		<label style="font-size: 1.1em;" for="mce-EMAIL">Be part of our mailing list and receive news bulletins!</label><br><br>
 		<input type="email" value="" name="EMAIL" class="required email" style="width: 100%;" id="mce-EMAIL" placeholder=" ▷ Simply enter here your email address, you can unsubscribe at any time ◁">
 	</div>
     <div style="display: inline-block; vertical-align: top;" class="lighten clear">
@@ -16,8 +17,10 @@
 	<div id="mce-responses" class="clear">
 		<div class="response" id="mce-error-response" style="display:none"></div>
 		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- Real people should not fill this in and expect good things - do not remove this or risk form bot signups -->
-    <div style="position: absolute; left: -5000px;"><input type="text" name="b_32f1406080b23a82ecf9e8d6b_94b74a4105" tabindex="-1" value=""></div>
+	</div>	<!-- Real people should not fill this in and expect good things - do not remove this or risk form bot signups -->
+    <div style="position: absolute; left: -5000px;">
+		<label for="human">Are you human?</label>
+		<input type="text" name="b_32f1406080b23a82ecf9e8d6b_94b74a4105" tabindex="-1" value="" id="human"></div>
     </div>
 </form>
 </div>
