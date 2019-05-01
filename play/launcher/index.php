95939b3eb1ae48317f8ca2925e2347aae9948657
DreadKnight <dk.vali@gmail.com>
2019-05-01 19:28:45 +0300

    no more scrolling over iframe, also some cleanup

--- play/launcher/index.php
+++ play/launcher/index.php
@@ -2,11 +2,10 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width">
 
 		<title>Ancient Beast</title>
-		<meta name="description" content="Ancient Beast is a turn based strategy indie game project.">
+		<meta name="description" content="Turn based strategy game.">
 
 		<style>
 		html, body {
@@ -63,10 +62,8 @@
 			<a href="https://www.twitter.com/AncientBeast" target="_blank">Twitter</a>
 			<a href="https://www.paypal.me/AncientBeast" target="_blank">Donate</a>
 			<a onclick="if(confirm('Reset Game?')) var ifr=document.getElementsByName('game')[0]; ifr.src=ifr.src;">Reset</a>
-			<a onclick="requestFullScreen()">Fullscreen</a>
+			<a onclick="game.requestFullscreen()">Fullscreen</a>
 		</div>
-		<iframe id="game" name="game" src="https://play.AncientBeast.com" seamless allowFullScreen></iframe>
+		<iframe id="game" name="game" src="https://play.AncientBeast.com" seamless allowFullScreen scrolling="none"></iframe>
 	</body>
 </html>
-
-<script src="fullscreen.js"></script>
