45a04aeb4fc45f929b2301c38ae7cba47b531b92
DreadKnight <dk.vali@gmail.com>
2020-06-13 15:10:46 +0300

    simplified links

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <br>
 </div>
 <div class="footer smalldiv center">
-	<a href="https://www.paypal.me/AncientBeast" target="_blank" rel="noopener">
+	<a href="https://paypal.me/AncientBeast" target="_blank" rel="noopener">
 		<img src="<?php echo $site_root; ?>images/footer/paypal.png" class="lighten" alt="paypal" title="Donate using PayPal!">
 	</a>
 	<a href="bitcoin:1ALLZzy3AZGvAuNso4Wca8SCx9YGXJdFGb?label=Ancient%20Beast">
 		<img src="<?php echo $site_root; ?>images/footer/bitcoin.png" style="width: 48px; height: 48px;" class="lighten" alt="bitcoin" title="Donate some Bitcoins!">
 	</a>
-	<a href="https://www.patreon.com/AncientBeast" target="_blank" rel="noopener">
+	<a href="https://patreon.com/AncientBeast" target="_blank" rel="noopener">
 		<img src="<?php echo $site_root; ?>images/footer/patreon.png" style="width: 48px; height: 48px;" class="lighten" alt="patreon" title="Pledge to our Patreon campaign!">
 	</a>
 	<a href="https://opencollective.com/AncientBeast" target="_blank" rel="noopener">
@@ -19,7 +19,7 @@
 	<a href="https://twitter.com/AncientBeast" target="_blank" rel="noopener">
 		<img src="<?php echo $site_root; ?>images/footer/twitter.png" style="width: 48px; height: 48px;" class="lighten" alt="twitter" title="Follow us on Twitter!">
 	</a>
-	<a href="https://www.youtube.com/AncientBeastGame" target="_blank" rel="noopener">
+	<a href="https://youtube.com/AncientBeastGame" target="_blank" rel="noopener">
 		<img src="<?php echo $site_root; ?>images/footer/youtube.png" style="width: 48px; height: 48px;" class="lighten" alt="youtube" title="Subscribe to our Youtube channel!">
 	</a>
 	<a href="https://github.com/FreezingMoon/AncientBeast" target="_blank" rel="noopener">
@@ -31,7 +31,7 @@
 	<a href="https://Ancient-Beast.deviantart.com" target="_blank" rel="noopener">
 		<img src="<?php echo $site_root; ?>images/footer/deviantart.png" style="width: 48px; height: 48px;" class="lighten" alt="deviantart" title="Join our deviantArt group!">
 	</a>
-	<a href="https://www.reddit.com/r/AncientBeast" target="_blank" rel="noopener">
+	<a href="https://reddit.com/r/AncientBeast" target="_blank" rel="noopener">
 		<img src="<?php echo $site_root; ?>images/footer/reddit.png" style="width: 48px; height: 48px;" class="lighten" alt="reddit" title="Subscribe to our Reddit!">
 	</a>
 	<a href="https://discord.me/AncientBeast" target="_blank" rel="noopener">
