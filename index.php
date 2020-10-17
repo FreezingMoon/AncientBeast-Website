8d0d50a726de9a8c4b83f016015193790a674bf5
DreadKnight <dk.vali@gmail.com>
2020-10-17 10:19:53 +0300

    online multiplayer workaround

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ require_once 'header.php';
 ?>
 
 <div class="center">
-	<iframe id="game" name="game" src="https://www.freezingmoon.org/AncientBeast/" style="border: 4px ridge; border-color: grey; width: 934px; height: 525px;" seamless webkitAllowFullScreen mozAllowFullScreen allowFullScreen></iframe>
+	<iframe id="game" name="game" src="http://freezingmoon.org/AncientBeast/" style="border: 4px ridge; border-color: grey; width: 934px; height: 525px;" seamless webkitAllowFullScreen mozAllowFullScreen allowFullScreen></iframe>
 </div>
 
 <div id="sponsors" class="center lighten">
