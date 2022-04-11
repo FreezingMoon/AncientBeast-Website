92c722f64920ec94e60532f3b9160eae3235d3c6
DreadKnight <dk.vali@gmail.com>
2022-04-11 23:16:25 +0300

    New buttons, changed order, deprecated BTC button

--- footer.php
+++ footer.php
@@ -1,44 +1,50 @@
 <br>
 </div>
 <div class="footer smalldiv center">
-	<a href="https://paypal.me/AncientBeast" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/paypal.png" class="lighten" alt="paypal" title="Donate using PayPal!">
+	<a href="0x21f53DB4D2eAcde70a2E4F0407D073C971eC1A33">
+		<img src="<?php echo $site_root; ?>images/footer/ethereum.png" style="width: 40px; height: 48px;" class="lighten" alt="ethereum" title="Donate ERC20 tokens to this address!">
 	</a>
-	<a href="bitcoin:1ALLZzy3AZGvAuNso4Wca8SCx9YGXJdFGb?label=Ancient%20Beast">
-		<img src="<?php echo $site_root; ?>images/footer/bitcoin.png" style="width: 48px; height: 48px;" class="lighten" alt="bitcoin" title="Donate some Bitcoins!">
-	</a>
-	<a href="https://patreon.com/AncientBeast" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/patreon.png" style="width: 48px; height: 48px;" class="lighten" alt="patreon" title="Pledge to our Patreon campaign!">
+	<a href="https://www.mintme.com/token/XatteR" target="_blank" rel="noopener">
+		<img src="<?php echo $site_root; ?>images/footer/mintme.png" style="width: 40px; height: 48px;" class="lighten" alt="mintme" title="Purchase our token, XatteR!">
 	</a>
 	<a href="https://opencollective.com/AncientBeast" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/opencollective.png" style="width: 48px; height: 48px;" class="lighten" alt="opencollective" title="Pledge to our OpenCollective!">
+		<img src="<?php echo $site_root; ?>images/footer/opencollective.png" style="width: 40px; height: 48px;" class="lighten" alt="opencollective" title="Pledge to our OpenCollective!">
+	</a>
+	<a href="https://patreon.com/AncientBeast" target="_blank" rel="noopener">
+		<img src="<?php echo $site_root; ?>images/footer/patreon.png" style="width: 40px; height: 48px;" class="lighten" alt="patreon" title="Pledge to our Patreon campaign!">
+	</a>
+	<a href="https://paypal.me/AncientBeast" target="_blank" rel="noopener">
+		<img src="<?php echo $site_root; ?>images/footer/paypal.png" style="width: 40px; height: 48px;" class="lighten" alt="paypal" title="Donate using PayPal!">
 	</a>
 	<a href="https://facebook.com/AncientBeast" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/facebook.png" style="width: 48px; height: 48px;" class="lighten" alt="facebook" title="Give us a like on Facebook!">
+		<img src="<?php echo $site_root; ?>images/footer/facebook.png" style="width: 40px; height: 48px;" class="lighten" alt="facebook" title="Give us a like on Facebook!">
 	</a>
 	<a href="https://twitter.com/AncientBeast" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/twitter.png" style="width: 48px; height: 48px;" class="lighten" alt="twitter" title="Follow us on Twitter!">
+		<img src="<?php echo $site_root; ?>images/footer/twitter.png" style="width: 40px; height: 48px;" class="lighten" alt="twitter" title="Follow us on Twitter!">
 	</a>
 	<a href="https://youtube.com/AncientBeastGame" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/youtube.png" style="width: 48px; height: 48px;" class="lighten" alt="youtube" title="Subscribe to our Youtube channel!">
+		<img src="<?php echo $site_root; ?>images/footer/youtube.png" style="width: 40px; height: 48px;" class="lighten" alt="youtube" title="Subscribe to our Youtube channel!">
 	</a>
 	<a href="https://github.com/FreezingMoon/AncientBeast" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/github.png" style="width: 48px; height: 48px;" class="lighten" alt="github" title="Check our Github project page!">
+		<img src="<?php echo $site_root; ?>images/footer/github.png" style="width: 40px; height: 48px;" class="lighten" alt="github" title="Check our Github project page!">
 	</a>
 	<a href="https://mega.co.nz/#F!GAJAjAzL!AhBUayQndZbH_j2IL2B-nA" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/mega.png" style="width: 48px; height: 48px;" class="lighten" alt="mega" title="Browse our free assets on Mega!">
+		<img src="<?php echo $site_root; ?>images/footer/mega.png" style="width: 40px; height: 48px;" class="lighten" alt="mega" title="Browse our free assets on Mega!">
 	</a>
 	<a href="https://Ancient-Beast.deviantart.com" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/deviantart.png" style="width: 48px; height: 48px;" class="lighten" alt="deviantart" title="Join our deviantArt group!">
+		<img src="<?php echo $site_root; ?>images/footer/deviantart.png" style="width: 40px; height: 48px;" class="lighten" alt="deviantart" title="Join our deviantArt group!">
 	</a>
 	<a href="https://reddit.com/r/AncientBeast" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/reddit.png" style="width: 48px; height: 48px;" class="lighten" alt="reddit" title="Subscribe to our Reddit!">
+		<img src="<?php echo $site_root; ?>images/footer/reddit.png" style="width: 40px; height: 48px;" class="lighten" alt="reddit" title="Subscribe to our Reddit!">
 	</a>
-	<a href="https://discord.me/AncientBeast" target="_blank" rel="noopener">
-		<img src="<?php echo $site_root; ?>images/footer/discord.png" style="width: 48px; height: 48px;" class="lighten" alt="discord" title="Join our Discord server!">
+	<a href="https://discord.gg/x78rKen" target="_blank" rel="noopener">
+		<img src="<?php echo $site_root; ?>images/footer/discord.png" style="width: 40px; height: 48px;" class="lighten" alt="discord" title="Join our Discord server!">
+	</a>
+	<a href="https://t.me/TheAncientBeast" target="_blank" rel="noopener">
+		<img src="<?php echo $site_root; ?>images/footer/telegram.png" style="width: 40px; height: 48px;" class="lighten" alt="telegram" title="Join our Telegram server!">
 	</a>
 	<a href="mailto:DreadKnight@FreezingMoon.org?Subject=I love this project!" target="_blank">
-		<img src="<?php echo $site_root; ?>images/footer/contact.png" style="width: 48px; height: 48px;" class="lighten" alt="contact" title="Send us an e-mail!">
+		<img src="<?php echo $site_root; ?>images/footer/contact.png" style="width: 40px; height: 48px;" class="lighten" alt="contact" title="Send us an e-mail!">
 	</a>
 </div>
 </body>
