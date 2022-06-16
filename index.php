e38ffb2005197729c17f236f9cb85d73d780e712
DreadKnight <dk.vali@gmail.com>
2022-06-16 17:27:56 +0300

    fixing lighten for sponsor banners

--- index.php
+++ index.php
@@ -30,13 +30,13 @@ require_once 'header.php';
 	<iframe id="game" name="game" src="https://www.freezingmoon.org/AncientBeast/" style="border: 4px ridge; border-color: grey; width: 934px; height: 525px;" seamless sandbox="allow-same-origin allow-forms allow-scripts allow-popups" webkitAllowFullScreen mozAllowFullScreen allowFullScreen></iframe>
 </div>
 
-<div id="sponsors" class="center lighten">
-		<a href="https://Raj.Bet" target="_blank" title="Rajbet - Online Casino India"><img src="images/sponsors/RajBet.png" alt="Rajbet"></a>
-		<a href="https://CasinosHunter.com/online-casinos" target="_blank" title="Casinos Hunter - Best Online Casinos Canada"><img src="images/sponsors/CasinosHunter.png" alt="CasinosHunter"></a>
-        <a href="https://OnlineCasinoHEX.ca" target="_blank" title="Online Casino HEX - Best Online Casinos"><img src="images/sponsors/OnlineCasinoHEX.png" alt="OnlineCasinoHEX"></a>
-		<a href="https://SlotsEmpire.com" target="_blank" title="Online Slots for Real Money | Play at Slots Empire Casino"><img src="images/sponsors/SlotsEmpire.png" alt="SlotsEmpire" width="150" height="49"></a>
-		<a href="https://Inkedin.com/free-spins-no-deposit" target="_blank" title="Free Spins No Deposit 🎖️ Best UK Slots Offers"><img src="images/sponsors/Inkedin.png" alt="Inkedin" width="155" height="49"></a>
-		<a href="https://TheInternetSlots.com/au" target="_blank" title="Best Online Casino in Australia for Real Money ❤️"><img src="images/sponsors/TheInternetSlots.png" alt="TheInternetSlots"></a>
+<div id="sponsors" class="center">
+		<a href="https://Raj.Bet" target="_blank" title="Rajbet - Online Casino India"><img src="images/sponsors/RajBet.png" alt="Rajbet" class="lighten"></a>
+		<a href="https://CasinosHunter.com/online-casinos" target="_blank" title="Casinos Hunter - Best Online Casinos Canada"><img src="images/sponsors/CasinosHunter.png" alt="CasinosHunter" class="lighten"></a>
+        <a href="https://OnlineCasinoHEX.ca" target="_blank" title="Online Casino HEX - Best Online Casinos"><img src="images/sponsors/OnlineCasinoHEX.png" alt="OnlineCasinoHEX" class="lighten"></a>
+		<a href="https://SlotsEmpire.com" target="_blank" title="Online Slots for Real Money | Play at Slots Empire Casino"><img src="images/sponsors/SlotsEmpire.png" alt="SlotsEmpire" width="150" height="49" class="lighten"></a>
+		<a href="https://Inkedin.com/free-spins-no-deposit" target="_blank" title="Free Spins No Deposit 🎖️ Best UK Slots Offers"><img src="images/sponsors/Inkedin.png" alt="Inkedin" width="155" height="49" class="lighten"></a>
+		<a href="https://TheInternetSlots.com/au" target="_blank" title="Best Online Casino in Australia for Real Money ❤️"><img src="images/sponsors/TheInternetSlots.png" alt="TheInternetSlots" class="lighten"></a>
 </div>
 
 <?php
