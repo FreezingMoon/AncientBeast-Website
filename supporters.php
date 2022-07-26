2905ca2ae97d7593307999ec0cf91200e8fdb994
DreadKnight <dk.vali@gmail.com>
2022-07-27 02:48:13 +0300

    supporter related details

--- supporters.php
+++ supporters.php
@@ -1,3 +1,5 @@
+<div class="div">
+
 <div id="sponsors" class="center">
 		<a href="https://Raj.Bet" target="_blank" title="Rajbet - Online Casino India"><img src="images/sponsors/RajBet.png" alt="Rajbet" class="lighten"></a>
         <a href="https://Inkedin.com/free-spins-no-deposit" target="_blank" title="Free Spins No Deposit 🎖️ Best UK Slots Offers"><img src="images/sponsors/Inkedin.png" alt="Inkedin" class="lighten"></a>
@@ -8,4 +10,12 @@
         <a href="https://OnlineCasinoHEX.ca" target="_blank" title="Online Casino HEX - Best Online Casinos"><img src="images/sponsors/OnlineCasinoHEX.png" alt="OnlineCasinoHEX" width="150" height="50" class="lighten"></a>
 		<a href="https://SlotsEmpire.com" target="_blank" title="Online Slots for Real Money | Play at Slots Empire Casino"><img src="images/sponsors/SlotsEmpire.png" alt="SlotsEmpire" width="150" height="50" class="lighten"></a>
 		<a href="https://reddogcasino.com/en/games/blackjack" target="_blank" title="Online Blackjack for Real Money or Free | Play at Red Dog"><img src="images/sponsors/RedDog.png" alt="RedDogCasino" width="150" height="50" class="lighten"></a>
+</div>
+
+<br>
+
+<div class="center">
+If you wish to have your banner here, become a <a href="https://opencollective.com/ancientbeast/contribute/backer-8021" target="_blank"><b>Backer</b></a> or even a <a href="https://opencollective.com/ancientbeast/contribute/sponsor-8022" target="_blank"><b>Sponsor</b></a>.
+</div>
+
 </div>
