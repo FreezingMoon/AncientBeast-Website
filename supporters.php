c2d36c8827ac2f5c8d75bad6617d11af6349293c
DreadKnight <dk.vali@gmail.com>
2022-09-17 19:35:27 +0300

    automatized banners for backers/sponsors 1:1 ratio

--- supporters.php
+++ supporters.php
@@ -1,22 +1,17 @@
 <div id="supporters" class="div">
 
 <div id="sponsors" class="center">
-		<a href="https://Raj.Bet" target="_blank" title="Rajbet - Online Casino India"><img src="images/sponsors/RajBet.png" alt="Rajbet" class="lighten"></a>
-        <a href="https://Inkedin.com/free-spins-no-deposit" target="_blank" title="Free Spins No Deposit 🎖️ Best UK Slots Offers"><img src="images/sponsors/Inkedin.png" alt="Inkedin" class="lighten"></a>
+	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/sponsor.svg?avatarHeight=123&button=false"></object>
 </div>
 <br>
 <div id="backers" class="center">
-		<a href="https://CasinosHunter.com/online-casinos" target="_blank" title="Casinos Hunter - Best Online Casinos Canada"><img src="images/sponsors/CasinosHunter.png" alt="CasinosHunter" width="150" height="50" class="lighten"></a>
-        <a href="https://OnlineCasinoHEX.ca" target="_blank" title="Online Casino HEX - Best Online Casinos"><img src="images/sponsors/OnlineCasinoHEX.png" alt="OnlineCasinoHEX" width="150" height="50" class="lighten"></a>
-		<a href="https://SlotsEmpire.com" target="_blank" title="Online Slots for Real Money | Play at Slots Empire Casino"><img src="images/sponsors/SlotsEmpire.png" alt="SlotsEmpire" width="150" height="50" class="lighten"></a>
-		<a href="https://reddogcasino.com/en/games/blackjack" target="_blank" title="Online Blackjack for Real Money or Free | Play at Red Dog"><img src="images/sponsors/RedDog.png" alt="RedDogCasino" width="150" height="50" class="lighten"></a>
-		<a href="https://casino-professor.com" target="_blank" title="Casino Professor - casino-professor.com"><img src="images/sponsors/CasinoProfessor.png" alt="CasinoProfessor" width="150" height="50" class="lighten"></a>
-	</div>
+	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/backer.svg?avatarHeight=80&button=false"></object>
+</div>
 
 <br>
 
 <div class="center">
-If you wish to have your banner here, become a <a href="https://opencollective.com/ancientbeast/contribute/backer-8021" target="_blank"><b>Backer</b></a> or even a <a href="https://opencollective.com/ancientbeast/contribute/sponsor-8022" target="_blank"><b>Sponsor</b></a>.
+If you wish to have your banner here, become a <a href="https://opencollective.com/ancientbeast/contribute/backer-8021" target="_blank"><b><u>Backer</u></b></a> or even a <a href="https://opencollective.com/ancientbeast/contribute/sponsor-8022" target="_blank"><b><u>Sponsor</u></b></a>.
 </div>
 
 </div>
