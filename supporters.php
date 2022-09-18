86905f5c3999b3d430584745e9f77ad8edbda119
DreadKnight <dk.vali@gmail.com>
2022-09-18 19:55:47 +0300

    dofollow links for Sponsors (workaround)

--- supporters.php
+++ supporters.php
@@ -1,7 +1,11 @@
 <div id="supporters" class="div">
 
 <div id="sponsors" class="center">
+	<!-- switched to manual until fix for https://github.com/opencollective/opencollective/issues/5968 is in place
 	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/sponsor.svg?avatarHeight=123&button=false"></object>
+	-->
+	<a href="https://inkedin.com/free-spins-no-deposit" target="_blank"><img src="https://images.opencollective.com/inkedin/40e0c14/logo/256.png" width=123px; heigth=123px;></a>
+	<a href="https://writersperhour.com/write-my-paper" target="_blank"><img src="https://images.opencollective.com/write-my-paper/5787d4b/logo/256.png" width=123px; heigth=123px;></a>
 </div>
 <br>
 <div id="backers" class="center">
