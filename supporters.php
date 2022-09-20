fe55f0385049dacdd872fb8d62b339f4db87979d
DreadKnight <dk.vali@gmail.com>
2022-09-21 00:46:43 +0300

    goread avatar size optimization

--- supporters.php
+++ supporters.php
@@ -6,7 +6,7 @@
 <br>
 <div id="backers" class="center">
 	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/backer.svg?avatarHeight=80&button=false"></object>
-	<a href="https://goread.io/buy-instagram-followers" target="_blank"><img src="https://images.opencollective.com/goread_io/eb6337d/logo/256.png" width=80px; heigth=80px;></a>
+	<a href="https://goread.io/buy-instagram-followers" target="_blank"><img src="https://images.opencollective.com/goread_io/eb6337d/logo/80.png" width=80px; heigth=80px;></a>
 </div>
 
 <br>
