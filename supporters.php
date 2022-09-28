814504a3a64b8daa7eba689d4478e01bb5d7c2b1
DreadKnight <dk.vali@gmail.com>
2022-09-28 16:06:56 +0300

    display unlimited number of sponsors&backers

--- supporters.php
+++ supporters.php
@@ -3,11 +3,11 @@
 <div id="sponsors" class="center">
 	<!-- Keep RajBet logo until end of 2023 if still not subscribbed -->
 	<a href="https://raj.bet" target="_blank"><img src="https://pbs.twimg.com/profile_images/1522830021999706112/Ub16XX3X_200x200.jpg" width=123px; heigth=123px; style="border-radius: 66px;"></a>
-	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/sponsor.svg?avatarHeight=123&button=false"></object>
+	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/sponsor.svg?avatarHeight=123&limit=unlimited&button=false"></object>
 </div>
 <br>
 <div id="backers" class="center">
-	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/backer.svg?avatarHeight=80&button=false"></object>
+	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/backer.svg?avatarHeight=80&limit=unlimited&button=false"></object>
 	<a href="https://goread.io/buy-instagram-followers" target="_blank"><img src="https://images.opencollective.com/goread_io/eb6337d/logo/80.png" width=80px; heigth=80px;></a>
 </div>
 
