f1b3702fbf635ec793f2e89fcc52335d643f6fc1
DreadKnight <dk.vali@gmail.com>
2023-03-15 21:51:44 +0200

    added parners, tweaked related divs

--- supporters.php
+++ supporters.php
@@ -1,24 +1,25 @@
-<div id="supporters" class="div">
-
-<div id="sponsors" class="center">
+<div id="sponsors" class="div center">
 	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/sponsor.svg?avatarHeight=123&width=890&limit=unlimited&button=false"></object>
-	<a href="https://BloodGrail.com" target="_blank"><img src="../images/sponsors/BloodGrail.png" width=123px; heigth=123px; style="border-radius: 66px;"></a>
-	<a href="https://ebay.co.uk/usr/dreadknight666" target="_blank"><img src="../images/sponsors/DreadKnight.png" width=123px; heigth=123px; style="border-radius: 66px;"></a>
-	<!-- Keep RajBet logo until end of 2023 if still not subscribbed -->
-	<a href="https://raj.bet" target="_blank"><img src="https://pbs.twimg.com/profile_images/1522830021999706112/Ub16XX3X_200x200.jpg" width=123px; heigth=123px; style="border-radius: 66px;"></a>
+	<br><br>
+	Huge thanks to all our sponsors! If you wish to have your banner here, become a <a href="https://opencollective.com/ancientbeast/contribute/sponsor-8022" target="_blank"><b><u>Sponsor</u></b></a> today.
 </div>
-<br>
-<div id="backers" class="center">
+
+<div id="backers" class="div center">
 	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/backer.svg?avatarHeight=80&width=890&limit=unlimited&button=false"></object>
+	<br><br>
+	Big thanks to all our backers! If you wish to have your banner here, become a <a href="https://opencollective.com/ancientbeast/contribute/backer-8021" target="_blank"><b><u>Backer</u></b></a> as well.
+</div>
+
+<div id="partners" class="div center">
+	<a href="https://qoddi.com" target="_blank"><img src="../images/sponsors/Qoddi.png" width=80px; heigth=80px;></a>
+	<a href="https://BloodGrail.com" target="_blank"><img src="../images/sponsors/BloodGrail.png" width=80px; heigth=80px; style="border-radius: 66px;"></a>
+	<a href="https://ebay.co.uk/usr/dreadknight666" target="_blank"><img src="../images/sponsors/DreadKnight.png" width=80px; heigth=80px; style="border-radius: 66px;"></a>
+	<!-- Keep RajBet logo until end of 2023 if still not subscribbed -->
+	<a href="https://raj.bet" target="_blank"><img src="https://pbs.twimg.com/profile_images/1522830021999706112/Ub16XX3X_200x200.jpg" width=80px; heigth=80px; style="border-radius: 66px;"></a>
 	<!-- Manually placed backers for follow-up links (SEO) until this OC issue https://github.com/opencollective/opencollective/issues/5968 is fixed -->
 	<a href="https://goread.io/buy-instagram-followers" target="_blank"><img src="https://images.opencollective.com/goread_io/eb6337d/logo/80.png" width=80px; heigth=80px;></a>
 	<a href="https://likewave.io/buy-instagram-likes" target="_blank"><img src="https://images.opencollective.com/likewave/a2a24ae/logo/80.png" width=80px; heigth=80px;></a>
+	<br><br>
+	Cheers to our partners. If you have something useful to provide to the project, feel free to send us an e-mail &#8595;
 </div>
-
-<br>
-
-<div class="center">
-If you wish to have your banner here, become a <a href="https://opencollective.com/ancientbeast/contribute/backer-8021" target="_blank"><b><u>Backer</u></b></a> or even a <a href="https://opencollective.com/ancientbeast/contribute/sponsor-8022" target="_blank"><b><u>Sponsor</u></b></a>.
-</div>
-
 </div>
