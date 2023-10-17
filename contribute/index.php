016f8bd6227fc6e82316968f7f11c95963431ed0
vneb05 <vincent.neeb@mni.thm.de>
2023-10-17 22:51:26 +0200

    Fixing the issue with inline CSS

--- contribute/index.php
+++ contribute/index.php
@@ -58,14 +58,14 @@ require_once('../header.php');
 	<!-- Right Side -->
 	<div class="card sideB" style="background-image: url(<?php echo $site_root; ?>images/cards/margin.png), url(<?php echo $site_root; ?>images/cards/G.jpg);">
 	  	<!-- Display main ways to contribute -->
-		<div class="section abilities contributeCard">
+		<div class="section abilities">
 
 			<div class="ability">
 				<a href="#donate">
 				<div id="first_icon" class="icon" style="background-image: url(<?php echo $site_root; ?>images/contribute/donate.svg);">
 					<div class="contour"></div>
 				</div>
-				<div class="wrapper">
+				<div class="wrapper" style="display: flex !important; align-items: center;">
 					<div class="info">
 						<h3 id="first_title" class="underline">Donate</h3>
 						<span class="desc" id="desc">You can make one time donations to the project or monthly recurring ones, which will be put to various great uses.</span>
@@ -79,7 +79,7 @@ require_once('../header.php');
 				<div id="second_icon" class="icon" style="background-image: url(<?php echo $site_root; ?>images/contribute/artwork.svg);">
 					<div class="contour"></div>
 				</div>
-				<div class="wrapper">
+                <div class="wrapper" style="display: flex !important; align-items: center;">
 					<div class="info">
 						<h3 id="second_title" class="underline">Artwork</h3>
 						<span class="desc" id="desc">You can donate existing artwork that you think it could nicely fit the project or offer your skills to create new stuff.</span>
@@ -93,7 +93,7 @@ require_once('../header.php');
 				<div id="third_icon" class="icon" style="background-image: url(<?php echo $site_root; ?>images/contribute/coding.svg);">
 					<div class="contour"></div>
 				</div>
-				<div class="wrapper">
+                <div class="wrapper" style="display: flex !important; align-items: center;">
 					<div class="info">
 						<h3 id="third_title" class="underline">Coding</h3>
 						<span class="desc" id="desc">We can always use an extra coding hand adding new functionality to the game or website and to squash bugs.</span>
@@ -107,7 +107,7 @@ require_once('../header.php');
 				<div id="fourth_icon" class="icon" style="background-image: url(<?php echo $site_root; ?>images/contribute/audio.svg);">
 					<div class="contour"></div>
 				</div>
-				<div class="wrapper">
+                <div class="wrapper" style="display: flex !important; align-items: center;">
 					<div class="info">
 						<h3 id="fourth_title" class="underline">Audio</h3>
 						<span class="desc" id="desc">Fast paced epic tracks can really set players in the mood for combat, while sound effects can empower abilities.</span>
@@ -121,7 +121,7 @@ require_once('../header.php');
 				<div id="fifth_icon" class="icon" style="background-image: url(<?php echo $site_root; ?>images/contribute/writing.svg);">
 					<div class="contour"></div>
 				</div>
-				<div class="wrapper">
+                <div class="wrapper" style="display: flex !important; align-items: center;">
 					<div class="info">
 						<h3 id="fifth_title" class="underline">Support</h3>
 						<span class="desc" id="desc">If none of the options above suit you but you still want to contribute to the project, there are many ways to do so!</span>
