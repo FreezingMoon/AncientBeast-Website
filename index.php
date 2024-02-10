b19412c298521ceedf7913f2663b9215b06412ef
DreadKnight <dk.vali@gmail.com>
2024-02-10 11:43:46 +0200

    end of GG19, big thanks for helping out! <3

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ require_once 'header.php';
 ?>
 
 <!-- Announcement -->
-<div class="center important"><b>Summoners, we need your help! Please donate $1+ to our <a href="https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-40" target="_blank"><u>Gitcoin campaign</u>, thanks!</a></b></div>
+<!-- <div class="center important"><b>Summoners, we need your help! Please donate $1+ to our <a href="https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-40" target="_blank"><u>Gitcoin campaign</u>, thanks!</a></b></div> -->
 
 <div class="center">
 	<iframe id="game" name="game" src="https://play.AncientBeast.com" style="border: 4px ridge; border-color: grey; width: 934px; height: 525px;" seamless sandbox="allow-same-origin allow-forms allow-scripts allow-popups" webkitAllowFullScreen mozAllowFullScreen allowFullScreen></iframe>
