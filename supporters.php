9fdafb5838ebec4870034c917d442ba38a27dba9
DreadKnight <dk.vali@gmail.com>
2024-11-06 22:42:43 +0200

    bypass OC widget "unlimited" bug

--- supporters.php
+++ supporters.php
@@ -1,11 +1,11 @@
 <div id="sponsors" class="div center">
-	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/sponsor.svg?avatarHeight=123&width=890&limit=unlimited&button=false"></object>
+	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/sponsor.svg?avatarHeight=123&width=890&button=false"></object>
 	<br><br>
 	Huge thanks to all our sponsors! If you wish to have your banner here, become a <a href="https://opencollective.com/ancientbeast/contribute/sponsor-8022" target="_blank"><b><u>Sponsor</u></b></a> today.
 </div>
 
 <div id="backers" class="div center">
-	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/backer.svg?avatarHeight=80&width=890&limit=unlimited&button=false"></object>
+	<object type="image/svg+xml" data="https://opencollective.com/ancientbeast/tiers/backer.svg?avatarHeight=80&width=890&button=false"></object>
 	<br><br>
 	Big thanks to all our backers! If you wish to have your banner here, become a <a href="https://opencollective.com/ancientbeast/contribute/backer-8021" target="_blank"><b><u>Backer</u></b></a> as well.
 </div>
