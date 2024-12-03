8a35e292bfc313d153ce3dc93cea8af0b4ff3e6f
DreadKnight <dk.vali@gmail.com>
2024-12-03 05:08:50 +0200

    q/acc info page, 1st draft

---
+++ blog/2024-12-2/index.php
@@ -0,0 +1,66 @@
+<?php
+/* Ancient Beast - Free Open Source Online PvP TBS: card game meets chess, with creatures.
+ * Copyright (C) 2007-2014  Valentin Anastase (a.k.a. Dread Knight)
+ *
+ * This file is part of Ancient Beast.
+ *
+ * Ancient Beast is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Ancient Beast is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses
+ *
+ * https://www.AncientBeast.com
+ * https://github.com/FreezingMoon/AncientBeast
+ * DreadKnight@FreezingMoon.org
+ */
+
+$page_title = "q/acc Accelerator";
+require_once("../../header.php");
+?>
+
+<div class="div" id="intro">
+<h2 class="indexheader"><a href="#intro">We're finally part of an accelerator</a></h2>
+
+<p>Greetings to all sinners! Ancient Beast 🐺 is the first project to get accepted in the first cohort of a state-of-the-art accelerator called <a href="https://qacc.giveth.io" target="_blank">q/acc</a>, even though I've applied after the deadline and we already have a token, <a href="https://mintme.com/token/XatteR" target="_blank"><b><u>XatteR</u><b></a>. This is a great opportunity, as it will allow us to have more resources at our disposal, like marketing, networking, partners and even a bit of funding, which can go a long way for us, hopefully managing to fix some of the current project bottlenecks and increase monthly income.</p>
+
+<p>I've had to do quite a few audio and/or video talks about the project so far, to varying number of people. And more will follow! You can hear the me presenting the project in this <a href="https://x.com/0xPolygon/status/1838261775663329287" target="_blank">X Space</a>. It was also great finding out more about the 7 other projects that were carefully selected for the accelerator from a few hundred aplications, as most of them are quite something, able to have a huge impact over humanity.</p>
+</div>
+
+<div class="div" id="tokens">
+<p>One of the conditions of the accelerator was to create a new token on Polygon zkEVM, which is a sidechain scaling solution that complements the Ethereum blockchain. I didn't wanted to kill the current token, XatteR, as a lot of time and work was already invested into it and it hs a few hundred holders already, so I've found a way to make things work in the favor of the project: since every tech is different and has pros and cons, I would have the tokens be complementary by having each one focus on different areas where they would perform better based on the pros, with only a bit of overlap ☯️.</p>
+</div>
+
+<div class="div" id="XatteR">
+<img src="XatteR.png">
+<p><a href="https://mintme.com/token/XatteR" target="_blank">XatteR</a>, with a total supply of 10 million tokens and ultra cheap gas fees, will primary used as a marketing tool, lower value project bounties and most importantly, as a replacement for in-game Plasma for online multiplayer matches that need to be secured. Token will overall be rather cheap, mineable and partially recycled if needed. You can read more about it <a href=
+"https://github.com/FreezingMoon/AncientBeast/wiki/XatteR" target="_blank">in the wiki</a>.</p>
+</div>
+
+<div class="div" id="BEAST">
+<img src="BEAST.png">
+<p><a href="https://q-acc.giveth.io/project/ancient-beast" target="_blank">BEAST</a> token will have about 6 million tokens minted (lower supply), but higher gas fee, as it resides on way more popular blockchain. It will be more stable value wise, so it's the recommended token for investors. It will be used for accesing extra game modes that involve tokenomics, purchasing digital and physical merchandise and for higher level project bounties.</p>
+</div>
+
+<div class="div" id="minting">
+<p>Amazing opportunity! You'll be able to mint the new BEAST token at a very advantageous ratio during the public round taking place between 6 until 20 December. The newly minted tokens have all sorts of safety mechanisms in place against rug-pulls, sybling attacks, whales and a lot of other stuff. The locking is way bigger for the projects and there's also a liquidity pool provided, so you'll be able to cash-out as the tokens unlock without taking a loss.</p>
+
+<p>Requirements: you need to be over 18, undergo a bit of KYC (two options available) and not be from USA, UK plus a few other handful of countries that are restricted, though if you're "donating" under $1000 worth of POL, you can bypass the country restriction at your own risk! For this first option, you can aim for at least 15 points within <a href="https://passport.xyz" target="_blank">passport.xyz</a>, which is quite nice and useful, or for larger amounts (over $1000, up to $15000), you can go for <a href="https://privado.id" target="_blank">Privado.id</a>, though this one requires a scan of your id and can take a day or two. You can find a very detailed guide about the KYC options over <a href="https://giveth.notion.site/Get-Verified-1223ab28d48c80458699d18cb0f54494" target="_blank">here</a>. There is also <a href="https://q-acc.giveth.io" target="_blank">support</a> provided in case you get stuck or face issues.</p>
+
+<p>Crypto: you will also need to have some ETH and POL on Polygon zkEVM, there's a nice easy to use widget for you to easily bridge and swap tokens if needed and you can also check out this handy guide <a href="https://giveth.notion.site/Move-ETH-and-POL-to-Polygon-zkEVM-1223ab28d48c8003b76fd98c3ed2a194" target="_blank">over here</a>.</p>
+
+<p>Also keep in mind that <a href="https://q-acc.giveth.io" target="_blank">the first cohort</a> is formed of <a href="https://q-acc.giveth.io/projects" target="_blank">8 projects</a>, so perhaps some of the other ones will peek your interest as well, just don't forget about <a href="https://q-acc.giveth.io/project/ancient-beast" target="_blank">BEAST</a> 😉</p>
+
+<p> You can find everything you need to know over here: https://q-acc.giveth.io</p>
+</div>
+
+<?php
+disqus();
+include('../../footer.php'); ?>
