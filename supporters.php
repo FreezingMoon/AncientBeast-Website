54f3ce6832bbb59c69dd06b49211d27cb7dd4b5f
DreadKnight <dk.vali@gmail.com>
2025-03-22 01:17:19 +0200

    added hover titles to the images

--- supporters.php
+++ supporters.php
@@ -11,18 +11,18 @@
 </div>
 
 <div id="partners" class="div center">
-	<a href="https://quickswap.exchange/#/swap?currency0=ETH&currency1=0x4191F84f66e0B7CC10370Ff47a0E2168E35b9Bdf&swapIndex=2" target="_blank"><img src="../images/partners/BEAST.png" width=80px; heigth=80px;></a>
-	<a href="https://Qoddi.com" target="_blank"><img src="../images/partners/Qoddi.png" width=80px; heigth=80px;></a>
-	<a href="https://about.me/DreadKnight" target="_blank"><img src="../images/partners/DreadKnight.png" width=80px; heigth=80px; style="border-radius: 66px;"></a>
-	<a href="https://game-icons.net" target="_blank"><img src="../images/partners/Game-Icons.png" width=80px; heigth=80px;></a>
-	<a href="https://OpenGameArt.org" target="_blank"><img src="../images/partners/OpenGameArt.png" width=80px; heigth=80px;></a>
-	<a href="https://discadia.com/server/AncientBeast" target="_blank"><img src="../images/partners/Discadia.png" width=80px; heigth=80px;></a>
+	<a href="https://quickswap.exchange/#/swap?currency0=ETH&currency1=0x4191F84f66e0B7CC10370Ff47a0E2168E35b9Bdf&swapIndex=2" target="_blank"><img src="../images/partners/BEAST.png" title="Purchase our token, BEAST! 🐺" width=80px; heigth=80px;></a>
+	<a href="https://Qoddi.com" target="_blank"><img src="../images/partners/Qoddi.png" title="Check out our hosting partner!" width=80px; heigth=80px;></a>
+	<a href="https://about.me/DreadKnight" target="_blank"><img src="../images/partners/DreadKnight.png" title="This is the project's creator!" width=80px; heigth=80px; style="border-radius: 66px;"></a>
+	<a href="https://game-icons.net" target="_blank"><img src="../images/partners/Game-Icons.png" title="The ability icons are from this website!" width=80px; heigth=80px;></a>
+	<a href="https://OpenGameArt.org" target="_blank"><img src="../images/partners/OpenGameArt.png" title="We use and share assets with OGA!" width=80px; heigth=80px;></a>
+	<a href="https://discadia.com/server/AncientBeast" target="_blank"><img src="../images/partners/Discadia.png" title="Our Discord server is a Premium here!" width=80px; heigth=80px;></a>
 	<!-- Keep RajBet logo until end of 2024 if still not subscribbed -->
-	<a href="https://raj.bet" target="_blank"><img src="https://pbs.twimg.com/profile_images/1522830021999706112/Ub16XX3X_200x200.jpg" width=80px; heigth=80px; style="border-radius: 66px;"></a>
+	<a href="https://raj.bet" target="_blank"><img src="https://pbs.twimg.com/profile_images/1522830021999706112/Ub16XX3X_200x200.jpg" title="These guys supported us big time!" width=80px; heigth=80px; style="border-radius: 66px;"></a>
 	<!-- Manually placed backers for follow-up links (SEO) until this OC issue https://github.com/opencollective/opencollective/issues/5968 is fixed -->
-	<a href="https://goread.io/buy-instagram-followers" target="_blank"><img src="https://images.opencollective.com/goread_io/eb6337d/logo/80.png" width=80px; heigth=80px;></a>
-	<a href="https://DogSwap.xyz" target="_blank"><img src="../images/partners/DogSwap.png" width=80px; heigth=80px;></a>
-	<a href="https://mintme.com/token/BaTs" target="_blank"><img src="../images/partners/BaTs.png" width=80px; heigth=80px;></a>
+	<a href="https://goread.io/buy-instagram-followers" target="_blank"><img src="https://images.opencollective.com/goread_io/eb6337d/logo/80.png" title="Check out one of our main backers!" width=80px; heigth=80px;></a>
+	<a href="https://DogSwap.xyz" target="_blank"><img src="../images/partners/DogSwap.png" title="This amazing DeFi is where XatteR is being traded!" width=80px; heigth=80px;></a>
+	<a href="https://mintme.com/token/BaTs" target="_blank"><img src="../images/partners/BaTs.png" title="Token by DreadKnight for synergy music project!" width=80px; heigth=80px;></a>
 	<br><br>
 	Cheers to our partners. If you have something useful to provide to the project, feel free to send us an e-mail &#8595;
 </div>
