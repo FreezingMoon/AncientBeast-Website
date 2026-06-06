3d362c6092e9aa0f515dfe892fdb8ec6b1c52440
DreadKnight <dk.vali@gmail.com>
2026-06-06 06:37:45 +0300

    can haz cake!

--- blog/2023-02-03/index.php
+++ blog/2023-02-03/index.php
@@ -31,6 +31,7 @@ require_once("../../header.php");
 
 <div class="div" id="intro">
 <h2 class="indexheader"><a href="#intro">Version 0.4 - released on 2nd February (2023)</a></h2>
+<a rel="pop" href="cake.jpg"><img class="shadow artwork" width=100% src="cake.jpg" title="Party Time!" alt="cake"></a>
 
 <p>Hello to all sinners!
 Version 0.4 of Ancient Beast has finally been released on GitHub, and it marks another meaningful step forward for the project. This build closes
